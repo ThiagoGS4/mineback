ac6c9cb1b66dcf4133536f6f95dcf7d014d30823
Thiago Garcia Santos <thigasan@gmail.com>
2025-11-20 10:38:06 -0300

    adjusts

--- config/ssh.js
+++ config/ssh.js
@@ -1,10 +1,22 @@
 import fs from 'fs';
 
+function getPrivateKey() {
+  if (process.env.SSH_PRIVATE_KEY) {
+    return process.env.SSH_PRIVATE_KEY;
+  }
+
+  if (process.env.SSH_KEY_PATH) {
+    return fs.readFileSync(process.env.SSH_KEY_PATH);
+  }
+
+  throw new Error('No SSH key configured');
+}
+
 const sshConfig = {
   host: process.env.SSH_HOST,
   port: 22,
   username: process.env.SSH_USER,
-  privateKey: fs.readFileSync(process.env.SSH_PRIVATE_KEY),
+  privateKey: getPrivateKey(),
 };
 
-export default sshConfig
+export default sshConfig;
